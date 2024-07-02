4d297266c52453cdebf7f3e7d900c951b8fc7f86
Alejandro Nelson <alejanel5@gmail.com>
2024-07-02 19:20:39 -0400

    Prueba con github

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,25 @@
 import { NavLink, Link } from "react-router-dom";
 
 import "../styles/NavBar.css";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 export const NavBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [fix, setFix] = useState(false);
+
+  function setFixed() {
+    if (window.scrollY > 0) {
+      setFix(true);
+    } else {
+      setFix(false);
+    }
+  }
+
+  window.addEventListener("scroll", setFixed);
 
   return (
-    <>
-      <nav>
+    < >
+      <nav className={fix ? "navBar_fixed" : ""}>
         <Link to="/">
           <span>Logo</span>
         </Link>
@@ -17,7 +28,7 @@ export const NavBar = () => {
           <span></span>
           <span></span>
         </div>
-        <ul className={menuOpen ? "open": ""}>
+        <ul className={menuOpen ? "open" : ""}>
           <li>
             <NavLink to="/">Home</NavLink>
           </li>
@@ -28,7 +39,7 @@ export const NavBar = () => {
             <NavLink to="/pagina2">Productos</NavLink>
           </li>
           <li>
-            <NavLink to="/55782">Error</NavLink>
+            <NavLink to="/Not_found">Error</NavLink>
           </li>
         </ul>
       </nav>
